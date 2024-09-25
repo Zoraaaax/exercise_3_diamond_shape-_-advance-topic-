408eb2a087539819911035bd193c9c074dbf491a
Kimberly Aguilar <ae.xerx@gmail.com>
2024-09-25 15:47:08 +0800

    loop_to_reverse_the_upper_half_of_the_pyramid

--- exercise_3_diamond_shape-_-advance-topic.py
+++ exercise_3_diamond_shape-_-advance-topic.py
@@ -8,4 +8,5 @@ def print_diamond():
         for i in range(n // 2 + 1):
             print(' ' * (n // 2 - i) + '*' * (2 * i + 1))
 
-        
+        for i in range(n // 2 - 1, -1, -1):
+            
