040fb3c40bf6a4fdb4cf5cca29e9f2b2e50c2852
Kimberly Aguilar <ae.xerx@gmail.com>
2024-09-26 21:44:59 +0800

    loop_through_half_the_range_of_the_odd_number

--- exercise_3_diamond_shape-_-advance-topic.py
+++ exercise_3_diamond_shape-_-advance-topic.py
@@ -7,7 +7,7 @@ def print_diamond():
         else:
             break
 
-        for i in range(n // 2 + 1):
+    for i in range(n // 2 + 1):
             print(' ' * (n // 2 - i) + '*' * (2 * i + 1))
 
         for i in range(n // 2 - 1, -1, -1):
