73c52a702e1fc14f05bfb78da4e68249b0fa1f5e
Kimberly Aguilar <ae.xerx@gmail.com>
2024-09-26 21:38:31 +0800

    prompt_for_an_odd_integer_if_input_is_even

--- exercise_3_diamond_shape-_-advance-topic.py
+++ exercise_3_diamond_shape-_-advance-topic.py
@@ -3,7 +3,7 @@ def print_diamond():
         n = int(input('Enter odd integer: '))
 
         if n % 2 == 0:
-        print('Please provide an odd integer.')
+            print('Please provide an odd integer.')
 
     else:
         for i in range(n // 2 + 1):
